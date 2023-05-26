e98c7535eaabf6fa69275b5dfc9171d7c30322b9
Logan Larson <61062214+logan-larson@users.noreply.github.com>
2023-05-26 07:03:43 -0500

    Library and episode card section

--- mobileapp/lib/screens/library.dart
+++ mobileapp/lib/screens/library.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mobileapp/widgets/episode_card_section.dart';
+import 'package:mobileapp/models/episode_model.dart';
+
+// TEMP
+import 'package:mobileapp/widgets/queue_episode_card.dart';
 
 class LibraryScreen extends StatefulWidget {
   const LibraryScreen({
@@ -10,8 +15,49 @@ class LibraryScreen extends StatefulWidget {
 }
 
 class _LibraryScreenState extends State<LibraryScreen> {
+  List<Widget> inboxEpisodes = [
+    QueueEpisodeCard(
+      EpisodeModel(
+        'Lex Fridman',
+        Duration(hours: 2, minutes: 37),
+        DateTime(2023, 3, 24),
+      ),
+    ),
+    QueueEpisodeCard(
+      EpisodeModel(
+        'Joe Rogan',
+        Duration(hours: 2, minutes: 37),
+        DateTime(2023, 3, 24),
+      ),
+    ),
+    QueueEpisodeCard(
+      EpisodeModel(
+        '2000s Kids',
+        Duration(hours: 2, minutes: 37),
+        DateTime(2023, 3, 24),
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Text('Library!!!');
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: [
+          Center(
+            child: Text(
+              'Library',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          EpisodeCardSection(
+            'Inbox',
+            Text('Screen?'),
+            inboxEpisodes,
+          ),
+        ],
+      ),
+    );
   }
 }
