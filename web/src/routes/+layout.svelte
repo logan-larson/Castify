c953544e86228b33e6fa27ab07f3e7c7a6794057
logan-larson <loganlarson20@gmail.com>
2023-08-28 16:03:08 -0500

    Added basic main layout

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -4,22 +4,60 @@
 
     let isAuthenticated = false;
 
+    let drawerOpen = true;
+
     onMount(() => {
         // Make API call to 
     });
+
+    function toggleDrawer() {
+        drawerOpen = !drawerOpen;
+    }
 </script>
 
-<!-- Header Bar -->
-    <!-- Drawer Control -->
-    <!-- App Name -->
-    <!-- App Name -->
+<div class="header">
+  <button on:click={toggleDrawer}>Toggle Drawer</button>
+  <h1>My SvelteKit App</h1> 
+</div>
 
+<div class="drawer" class:open={drawerOpen}>
+  <!-- Add drawer content here -->
+  <p>Drawer Content</p>
+</div>
 
-<!-- Drawer -->
+<div class="content" class:shifted={!drawerOpen}>
+    <slot></slot>
+</div>
 
+<style>
+  .header {
+    background-color: #333;
+    color: white;
+    padding: 1rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
+  .drawer {
+    width: 250px;
+    height: 100vh;
+    background-color: #f5f5f5;
+    transition: transform 0.3s ease;
+    transform: translateX(-100%);
+  }
 
-<!-- Main -->
+  .drawer.open {
+    transform: translateX(0);
+  }
 
+  .content {
+    margin-left: 250px;
+    padding: 2rem;
+  }
 
+  .content.shifted {
+    margin-left: 0;
+  }
+</style>
 
