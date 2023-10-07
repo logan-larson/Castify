3158fcbffaf499baa6a2a24bf972a5ec927b88be
Logan Larson <loganlarson20@gmail.com>
2023-10-07 13:11:02 -0500

    Minor change to docker startup for neo4j local development

--- database/neo4j-docker.sh
+++ database/neo4j-docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Check if the container exists
-container_exists=$(docker ps -a | grep 'castify')
-container_running=$(docker ps | grep 'castify')
+container_exists=$(docker ps -a | grep 'castify-neo4j-1')
+container_running=$(docker ps | grep 'castify-neo4j-1')
 
 if [ -z "$container_exists" ]; then
-    docker run --name castify -d -p 7474:7474 -p 7687:7687 -v $HOME/neo4j/data:/data neo4j
+    docker run --name neo4j -d -p 7474:7474 -p 7687:7687 -v $HOME/neo4j/data:/data castify-neo4j-1
 else if [ -z "$container_running" ]; then
-    docker start castify
+    docker start castify-neo4j-1
 fi
