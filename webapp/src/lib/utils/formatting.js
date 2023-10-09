34e504f9b0f9ebed0ff07303738bfb90996123d6
Logan Larson <loganlarson20@gmail.com>
2023-10-09 07:49:14 -0500

    Fixed const reassign error

--- webapp/src/lib/utils/formatting.js
+++ webapp/src/lib/utils/formatting.js
@@ -1,8 +1,8 @@
 export const format = (seconds) => {
 	if (isNaN(seconds)) return '0:00';
 
-	const hours = Math.floor(seconds / 3600);
-	const minutes = Math.floor((seconds - (hours * 3600)) / 60);
+	let hours = Math.floor(seconds / 3600);
+	let minutes = Math.floor((seconds - (hours * 3600)) / 60);
 	seconds = Math.floor(seconds % 60);
 
 	if (seconds < 10)
