26be0744a26f4fe535ea73aba3123f1f455255bf
Logan Larson <loganlarson20@gmail.com>
2023-10-19 15:16:02 -0500

    Fixed docker compose for new server

--- database/neo4j-docker.sh
+++ database/neo4j-docker.sh
@@ -5,7 +5,9 @@ container_exists=$(docker ps -a | grep 'neo4j-castify')
 container_running=$(docker ps | grep 'neo4j-castify')
 
 if [ -z "$container_exists" ]; then
-    docker run --name neo4j -d -p 7474:7474 -p 7687:7687 -v $HOME/neo4j/data:/data neo4j-castify
-else if [ -z "$container_running" ]; then
-    docker start neo4j-castify
+    docker run --name neo4j-castify -d -p 7474:7474 -p 7687:7687 -v $HOME/neo4j/data:/data neo4j
+else
+    if [ -z "$container_running" ]; then
+        docker start neo4j-castify
+    fi
 fi
